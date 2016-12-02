328c683a9380b9fad223f50518a1691944a0660b
DragoescuGeorgiana <DragoescuGeorgiana@users.noreply.github.com>
2016-12-02 20:17:18 +0200

    Add files via upload

---
+++ Maxim.java
@@ -0,0 +1,22 @@
+package if_else;
+
+// Sa se calculeze maximul a doua numere
+
+public class Maxim {
+
+	public static void main(String[] args) {
+		int a, b, m;
+
+		a = 12;
+		b = 5;
+
+		if (a > b)
+			m = a;
+		else
+			m = b;
+
+		System.out.println("Maximul dintre" + " "  + a + " " +  "si" + " " + b + " "+  "este:" + " " + m);
+
+	}
+
+}
